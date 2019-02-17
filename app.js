52029cfbe65825b1cf58213d1ba48fb3ab42d542
Leonardo Bissaro <leonbissaro@gmail.com>
2019-02-17 22:50:20 +0100

    quick fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ display.clear_screen();
 //Start with the zero on the screen
 calculator.only_display("0");
 
-//Event delegation for the numbers with sounds
+//Event delegation for the keys
 document.getElementById("numeric_keys").addEventListener("click",function(e) {
     if(e.target && e.target.nodeName == "BUTTON") {
         //Compose string in memory and draw it
@@ -18,13 +18,11 @@ document.getElementById("numeric_keys").addEventListener("click",function(e) {
     }
 });
 
-//Event delegation for the service keys (sounds)
 document.getElementById("service_keys").addEventListener("click",function(e) {
     if(e.target && e.target.nodeName == "BUTTON") {
     }
 });
 
-//Event delegation for the operation keys (sounds)
 document.getElementById("operation_keys").addEventListener("click",function(e) {
     if(e.target && e.target.nodeName == "BUTTON") {
     }
